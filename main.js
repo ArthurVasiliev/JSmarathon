652829f41d3e48466bb02f23fbf4d158141b26e2
ArthurVasiliev <arthur.ensh@gmail.com>
2020-09-02 03:30:10 +0300

    Home Work 7

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
+import Pokemon from "./js/pokemon.js"
+
 const $btnstrike = document.getElementById('btn-kick');
 const $btnheal = document.getElementById('btn-heal');
-$btnstrike.getElementsByTagName('span')[1];
-const character = {
+
+const player1 = new Pokemon({
 name: 'Pikachu',
-defaultHP: 200,
-damagedHP: 200,
-elHP: document.getElementById('health-character'),
-elHPBar: document.getElementById('progressbar-character'),
-renderHP: renderHP,
-strike: strike,
-heal: heal,
-}
-const enemy = {
+type: 'Electric',
+hp: 200,
+selector: 'character',
+})
+
+const player2 = new Pokemon({
 name: 'Charmander',
-defaultHP: 200,
-damagedHP: 200,
-elHP: document.getElementById('health-enemy'),
-elHPBar: document.getElementById('progressbar-enemy'),
-renderHP: renderHP,
-strike: strike,
-heal: heal,
-}
+type: 'Fire',
+hp: 200,
+selector: 'enemy',
+})
 
 
 let cntKick = 10;
@@ -28,79 +23,57 @@ let cntHeal = 3;
 
 function init(){
 console.log('Start game!');
-character.renderHP();
-enemy.renderHP();
 $btnstrike.innerText = `Thunder Jolt [${cntKick}]`;
 $btnheal.innerText = `Heal [${cntHeal}]`;
 }
 
-$btnstrike.addEventListener('click', function(){
-
+$btnstrike.addEventListener('click', function(dmg){
 console.log('Strike');
-character.strike (rnd(20));
-enemy.strike (rnd(20));
+player1.strike (rnd(20), function(dmg){
+let p1dmg = generateLogDmg(player1, player2, dmg);
+const $logs = document.querySelector('#logs');
+const $p = document.createElement('p');
+$p.style.color = 'red';
+$p.innerHTML = p1dmg;
+$logs.insertBefore($p, $logs.children[0]);
+});
+player2.strike (rnd(20), function(dmg){
+let p2dmg = generateLogDmg(player2, player1, dmg)
+const $logs = document.querySelector('#logs');
+const $p = document.createElement('p');
+$p.style.color = 'red';
+$p.innerHTML = p2dmg;
+$logs.insertBefore($p, $logs.children[0]);
 })
+});
 $btnstrike.addEventListener('click', count($btnstrike, cntKick))
+
 $btnheal.addEventListener('click', function()
 {
-
   console.log('Heal');
-  character.heal (rnd(20));
-  enemy.heal (rnd(20));
+
+  player1.heal (rnd(20), function(hl){
+let p1hl = generateLogHeal(player1,hl)
+const $logs = document.querySelector('#logs');
+const $p = document.createElement('p');
+$p.style.color = 'blue';
+$p.innerHTML = p1hl;
+$logs.insertBefore($p, $logs.children[0]);
+})
+
+player2.heal (rnd(20), function(hl){
+let p2hl = generateLogHeal(player2,hl)
+const $logs = document.querySelector('#logs');
+const $p = document.createElement('p');
+$p.style.color = 'blue';
+$p.innerHTML = p2hl;
+$logs.insertBefore($p, $logs.children[0]);
+})
+
 
 })
 $btnheal.addEventListener('click', count($btnheal, cntHeal))
-function renderHP(){
 
-this.elHP.innerText = this.damagedHP + ' / ' + this.defaultHP;
-this.elHPBar.style.width = (this.damagedHP / (this.defaultHP/100)) + '%';
-}
-
-function strike(dmg){
-this.damagedHP -= dmg;
-const log = this === enemy ? generateLogDmg(this, character, dmg) : generateLogDmg(this, enemy, dmg);
-const $logs = document.querySelector('#logs');
-const $p = document.createElement('p');
-$p.innerHTML = log;
-$logs.insertBefore($p, $logs.children[0]);
-
-
-  if (this.damagedHP <= dmg)
-  {
-
-this.damagedHP = 0;
-alert(this.name + ' сражался достойно, но проиграл!')
-$btnstrike.disabled = true;
-  }
-this.renderHP();
-}
-
-function heal(hl){
-let t = 0;
-if (t === 3)
-{
-
-$btnheal.disabled = true;
-
-}
-
-if(this.defaultHP <= (this.damagedHP + hl))
-{
-
-this.damagedHP = this.defaultHP;
-
-}
-else{
-
-this.damagedHP += hl;
-
-}
-const $logs = document.querySelector('#logs');
-const $p = document.createElement('p');
-$p.innerHTML = generateLogHeal(this, hl);
-$logs.insertBefore($p, $logs.children[0]);
-this.renderHP();
-}
 
 const rnd = (num) => Math.ceil(Math.random()*num);
 
@@ -108,7 +81,7 @@ function generateLogDmg(firstCharacter, secondCharacter, dmg){
   function lifelog()
   {
 
-  return `- ${dmg}, [${firstCharacter.damagedHP} / ${firstCharacter.defaultHP}]`;
+  return `- ${dmg}, [${firstCharacter.hp.current} / ${firstCharacter.hp.total}]`;
 
   }
 
@@ -137,7 +110,7 @@ function generateLogHeal(character,hl)
   function lifelog()
   {
 
-  return `+ ${hl}, [${character.damagedHP} / ext${character.defaultHP}]`;
+  return `+ ${hl}, [${character.hp.current} / ${character.hp.total}]`;
 
   }
 const logs = [
@@ -163,17 +136,11 @@ return function(){
   $btn.innerText = `Thunder Jolt [${clickNumb - num}]`;
 }
 else if($btn === $btnheal){
-
 $btn.innerText = `Heal [${clickNumb - num}]`;
-
 }
-  if (num >= clickNumb){
-
-$btn.disabled = true;
-
-  }
-
+if (num >= clickNumb){
+    $btn.disabled = true;
 }
 }
-
+}
 init()
